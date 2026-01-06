46e82b172c384f60dd1212a2669bc206ce6953cb
thomaslucking <telucking@gmail.com>
2026-01-06 10:26:03 +0100

    feat: created routes and GET method closes #10

--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
 
 import index from './index.html'; 
+import { reqGET } from './db.ts';
 
 const server = Bun.serve({
   port: 3000,
   routes: {
     "/": index, 
-    
+    "/api/todos": (req)=> {
+      if(req.method === "GET"){
+        const todos = reqGET()
+
+        return Response.json(todos)
+      }
+
+      return new Response('Method does not exist')
+
+    }
   }
+
 });
 
 console.log(`Listening on ${server.url}`);
